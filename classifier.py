247907d30cb61284a5eb16ad1ac63dadc2db5d4c
Rob <basorot@gmail.com>
2019-05-15 13:06:27 +0900

    ADDED classifier file which will contains classification code

---
+++ classifier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Created on May 15, 2019
+@author: Robert BASOMINGERA
+@Ajou University
+This project is developed and tested with Python3.5 using pycharm on an Ubuntu 16.04 LTS machine
+'''
+
+if __name__ == '__main__':
+    pass
