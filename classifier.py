4396a95542c7f630e038d83e3778e4ee0170d84a
Rob <basorot@gmail.com>
2019-08-05 09:38:34 +0900

    ADDED sequential model

--- classifier.py
+++ classifier.py
@@ -17,12 +17,19 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import Pipeline
 
 from keras.preprocessing import image
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, InputLayer
 import numpy as np
 import os
 import pandas as pd
 import random
 import json
-from sklearn.metrics import accuracy_score
+import keras
+from sklearn.metrics import accuracy_score, log_loss, brier_score_loss, roc_auc_score
+
+from keras.layers import Dropout
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
 
 # from PIL import Image
 import matplotlib.pyplot as plt
@@ -34,8 +41,41 @@ JSON_LABEL_PATH = "./flowers.json"
 vgg19_weights = './weights/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5'
 vgg19_model = VGG19(weights=vgg19_weights, include_top=False)
 
+
 def init():
     pass
+
+
+def get_labels():
+    # read to ensure they are real there
+    raw_labels = os.listdir("./data/")
+    # labels_name = []
+    all_labels = []
+    for this_label in raw_labels:
+        all_labels.append(labels_of_id[this_label]['id'])
+
+    # print("labels", all_labels, raw_labels)
+    return all_labels
+
+
+def initialize_images_data(number_of_labels):
+    this_data = {}
+    this_test_images = {}
+    this_expected_labels = []
+
+    for i in range(number_of_labels):
+        this_data[labels[i]] = os.listdir("./data/" + id_of_labels[labels[i]] + "/")
+        this_test_images[labels[i]] = []
+
+        for j in range(len(this_data[labels[i]]) // 4):
+            # print(labels[i], len(data[labels[i]]))
+            temp_flower = random.randint(0, len(this_data[labels[i]]) - 1)
+            this_expected_labels.append(labels[i])
+
+            this_test_images[labels[i]].append(this_data[labels[i]][temp_flower])
+            # remove images added to the test images
+            del this_data[labels[i]][temp_flower]
+    return this_data, this_test_images, this_expected_labels
 
 
 def get_json_label_id():
@@ -67,69 +107,68 @@ def get_features(img_path, this_model):
     img_features = this_model.predict(img_data)
     return img_features
 
+
+def extract_features():
+    this_features = {}
+
+    # initialize  dict of features
+    # TODO use if to check instead of a for loop
+    for x in range(nber_labels):
+        this_features[labels[x]] = []
+
+    for label, flowers in data.items():
+        basepath = "./data/" + id_of_labels[label]
+
+        for flower in flowers:
+            feats = get_features(basepath + "/" + flower, vgg19_model)
+            # flatten the features
+            this_features[label].append(feats.flatten())
+            # squeeze the features
+            # this_features[label].append(feats.squeeze())
+    return this_features
+
 init()
 id_of_labels = id_to_label_dict()
 labels_of_id = get_json_label_id()
-labels = []
-
-# read to ensure they are real there
-raw_labels = os.listdir("./data/")
-# labels_name = []
-for this_label in raw_labels:
-    labels.append(labels_of_id[this_label]['id'])
-
-
-print("labels", labels, raw_labels)
+labels = get_labels()
 nber_labels = len(labels)
 
 # initialize a dict of labels and the data set
-data = {}
-test_images = {}  # for testing
-expected_labels = []
+data, test_images, expected_labels = initialize_images_data(nber_labels)
 
-for i in range(nber_labels):
-    data[labels[i]] = os.listdir("./data/"+id_of_labels[labels[i]]+"/")
-    test_images[labels[i]] = []
-
-    for j in range(len(data[labels[i]])//4):
-        # print(labels[i], len(data[labels[i]]))
-        temp_flower = random.randint(0, len(data[labels[i]])-1)
-        expected_labels.append(labels[i])
-
-        test_images[labels[i]].append(data[labels[i]][temp_flower])
-        # remove images added to the test images
-        del data[labels[i]][temp_flower]
-
-
-# initialize  dict of features
-# TODO use if to check instead of a for loop
-features = {}
-for x in range(nber_labels):
-    features[labels[x]] = []
-
-for label, flowers in data.items():
-    basepath = "./data/"+id_of_labels[label]
-
-    for flower in flowers:
-        feats = get_features(basepath + "/" + flower, vgg19_model)
-        # flatten the features
-        features[label].append(feats.flatten())
-        # squeeze the features
-        # features[label].append(feats.squeeze())
+features = extract_features()
 
 # convert features to data frame (from dict)
-dataset = pd.DataFrame()
+trainY = list()  # labels
+trainX = list() # pd.DataFrame()  # images data
+
 for label, feats in features.items():
-    temp_df = pd.DataFrame(feats)
-    temp_df['label'] = label
-    dataset = dataset.append(temp_df, ignore_index=True)
-dataset.head()
+    for i in range(len(feats)):
+        trainY.append([label]) # temp_label, ignore_index=True)
+    # temp_df = pd.DataFrame(feats)
+    # temp_label = pd.DataFrame(label)
+
+    # temp_df['label'] = label
+    # dataset = dataset.append(temp_df, ignore_index=True)
+    # trainX.append(feats)
+    trainX.append(feats)
+    # trainX = trainX.append(temp_df, ignore_index=True)
+
+# dataset.head()
+# trainX.head()
+# trainY = pd.DataFrame(trainY)
+# trainY = keras.utils.to_categorical(trainY)
+
+# trainY.head()
+# print(len(trainX))
+# print(type(trainY))
+
 
 # get test images features
 test_features = []
 test_images_paths = []
 for test_label, test_flowers in test_images.items():
-    basepath = "./data/"+id_of_labels[test_label]
+    basepath = "./data/" + id_of_labels[test_label]
 
     for test_flower in test_flowers:
         test_flower_path = basepath + "/" + test_flower
@@ -139,16 +178,94 @@ for test_label, test_flowers in test_images.items():
         test_features.append(test_feats.flatten())
         # test_features.append(test_feats.squeeze())
 
-y = dataset.label
-x = dataset.drop('label', axis=1)
+# y = dataset.label
+# x = dataset.drop('label', axis=1)
 
 # classifier model
-model = MLPClassifier(hidden_layer_sizes=(100, 10))
-pipeline = Pipeline([('low_variance_filter', VarianceThreshold()), ('model', model)])
-pipeline.fit(x, y)
+# model = MLPClassifier(hidden_layer_sizes=(10, 100))
+# model = Sequential()
+# model.add(Dense(100, activation='softmax', name='fc2'))
+# model.add(Dense(len(labels), kernel_initializer="uniform", activation='softmax'))
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# pipeline = Pipeline([('low_variance_filter', VarianceThreshold()), ('model', model)])
+# pipeline.fit(trainX, keras.utils.to_categorical(trainY))
+
+classifier = Sequential()
+
+# n_timesteps, n_features, n_outputs = (trainX).shape[0], (trainX).shape[1], (trainY).shape[0]
+# print(n_timesteps, n_features, n_outputs)
+#
+# classifier.add(InputLayer(input_shape=(trainX.shape[1],)))
+
+classifier.add(Conv1D(filters=64, kernel_size=2, activation='relu', name='conv1D1'))
+# classifier.add(Conv1D(filters=64, kernel_size=3, activation='relu', batch_input_shape=(0, n_timesteps, n_features), name='conv1D1'))
+classifier.add(Conv1D(filters=64, kernel_size=3, activation='relu', name='conv2'))
+classifier.add(Dropout(0.5))
+classifier.add(MaxPooling1D(pool_size=2))
+
+classifier.add(Dense(128, name='ds0', activation='relu'))  # , input_dim=11))
+classifier.add(Dropout(0.3))
+classifier.add(Dense(100, activation='relu', name='ds1'))  # , input_shape=(,)))
+classifier.add(Dropout(0.3))
+classifier.add(Dense(100, activation='softmax', name='ds2'))
+
+classifier.add(Flatten())
+classifier.add(Dense(max(labels)+1, name='ds3', activation='softmax'))  # softmax  sigmoid
+classifier.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy']) # rmsprop adam
+
+
+classifier.fit(trainX, trainY)
+score = classifier.evaluate(test_features, expected_labels)
+print(score)
+
+
+pipeline = Pipeline([('low_variance_filter', VarianceThreshold()), ('model', classifier)])
+# pipeline.set_params(steps_per_epoch=800, epochs=10)
+pipeline.fit(trainX, trainY)  #  , steps_per_epoch=80, epochs=10, batch_size=128)
+
+# print(type(list(trainX)))
+# classifier.fit({'input': list(trainX)}, {'targets': keras.utils.to_categorical(trainY)}, steps_per_epoch=800, epochs=10)
+               # validation_data=(test_features.shape, expected_labels.shape), steps_per_epoch=800, epochs=10)
+
+# another model
+# verbose, epochs, batch_size = 0, 10, 32
+# print(trainX.shape)
+# n_timesteps, n_features, n_outputs = trainX.shape[0], trainX.shape[1], trainY.shape[0]
+# model = Sequential()
+# model.add(Flatten())
+# model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps, n_features), name='conv1'))
+# model.add(Conv1D(filters=64, kernel_size=3, activation='relu', name='conv2'))
+# model.add(Dropout(0.5))
+# model.add(MaxPooling1D(pool_size=2))
+#
+# model.add(Dense(100, activation='relu'))
+#
+# model.add(Dense(n_outputs, activation='softmax'))
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# # fit network
+# pipeline2 = Pipeline([('low_variance_filter', VarianceThreshold()), ('model', model)])
+# pipeline2.fit(trainX, trainY)  #  , epochs=epochs, batch_size=batch_size, verbose=verbose)
+# # evaluate model
+# _, accuracy = pipeline2.predict(test_features, expected_labels, batch_size=batch_size, verbose=0)
+# print(accuracy, accuracy)
+
+
+
+# print(pipeline.(test_features))
+predicted_labels = pipeline.predict(test_features)
+print(predicted_labels)
 
 # detection process
-predicted_labels = pipeline.predict(test_features)
+# predicted_labels = pipeline.predict(test_features)
+
+
+# loss = log_loss(expected_labels, predicted_labels)
+# print("loss:", loss)
+# brier_loss = brier_score_loss(expected_labels, predicted_labels)
+# print("brier", brier_loss)
+# calculate roc curve
+fpr, tpr, thresholds = roc_auc_score (expected_labels, predicted_labels)
+
 
 the_score = accuracy_score(expected_labels, predicted_labels)
 print("Accuracy: ", the_score * 100, "%")
