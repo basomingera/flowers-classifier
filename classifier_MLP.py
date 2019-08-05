3db0c0dce978d4fbe80e38f4b0e795b38cf1b894
Rob <basorot@gmail.com>
2019-08-05 09:40:27 +0900

    UPDATED differentiate test and train data

--- classifier_MLP.py
+++ classifier_MLP.py
@@ -25,16 +25,21 @@ import json
 import keras
 from sklearn.metrics import accuracy_score
 
-# from PIL import Image
+from PIL import Image
 import matplotlib.pyplot as plt
 
+import umap
+from ripser import Rips
+
 DEBUG = False
-JSON_LABEL_PATH = "./flowers.json"
+# JSON_LABEL_PATH = "./flowers.json"
+JSON_LABEL_PATH = "./flower_data/cat_to_name.json"
 
 # VGG19
 vgg19_weights = './weights/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5'
 vgg19_model = VGG19(weights=vgg19_weights, include_top=False)
 
+test_images_paths = []
 
 def init():
     pass
@@ -42,32 +47,23 @@ def init():
 
 def get_labels():
     # read to ensure they are real there
-    raw_labels = os.listdir("./data/")
+    raw_labels = os.listdir("./flower_data/train/")
     # labels_name = []
     all_labels = []
     for this_label in raw_labels:
-        all_labels.append(labels_of_id[this_label]['id'])
+        # all_labels.append(labels_of_id[this_label]['id'])
+        all_labels.append(labels_of_id[this_label])
 
     # print("labels", all_labels, raw_labels)
     return all_labels
 
 
-def initialize_images_data(number_of_labels):
+def initialize_images_data(number_of_labels, directory):
     this_data = {}
-    this_test_images = {}
 
     for i in range(number_of_labels):
-        this_data[labels[i]] = os.listdir("./data/" + id_of_labels[labels[i]] + "/")
-        this_test_images[labels[i]] = []
-
-        for j in range(len(this_data[labels[i]]) // 4):
-            # print(labels[i], len(data[labels[i]]))
-            temp_flower = random.randint(0, len(this_data[labels[i]]) - 1)
-
-            this_test_images[labels[i]].append(this_data[labels[i]][temp_flower])
-            # remove images added to the test images
-            del this_data[labels[i]][temp_flower]
-    return this_data, this_test_images
+        this_data[labels[i]] = os.listdir("./flower_data/" + directory + "/" + id_of_labels[labels[i]] + "/")
+    return this_data
 
 
 def get_json_label_id():
@@ -80,7 +76,8 @@ def id_to_label_dict():
     json_label = get_json_label_id()
     flower_dict = {}
     for each_flower in json_label:
-        flower_dict[json_label[each_flower]['id']] = each_flower
+        # flower_dict[json_label[each_flower]['id']] = each_flower
+        flower_dict[json_label[each_flower]] = each_flower
     return flower_dict
 
 
@@ -100,21 +97,43 @@ def get_features(img_path, this_model):
     return img_features
 
 
-def extract_features():
+def extract_features(this_data, basedirectory):
     this_features = {}
+    this_labels = {}
 
     # initialize  dict of features
     # TODO use if to check instead of a for loop
     for x in range(nber_labels):
-        this_features[labels[x]] = []
+        this_features[id_of_labels[labels[x]]] = []
 
-    for label, flowers in data.items():
-        basepath = "./data/" + id_of_labels[label]
+    for label, flowers in this_data.items():
+        basepath = "./flower_data/" + basedirectory + "/" + id_of_labels[label]
 
         for flower in flowers:
             feats = get_features(basepath + "/" + flower, vgg19_model)
+
+            if basedirectory == "valid":
+                test_images_paths.append(basepath + "/" + flower)
+
+            # this_labels.append(int(id_of_labels[label]))
             # flatten the features
-            this_features[label].append(feats.flatten())
+            # this_features[id_of_labels[label]].append(feats.flatten())
+
+            # # print(feats.shape)
+            feats_reshaped = np.reshape(feats, (feats.shape[2], -1))
+            # print(feats_reshaped)
+            # # print()
+            # # TDA use of UMAP
+            # print(feats_reshaped.shape)
+            # feats_frame = pd.DataFrame(feats)
+            # feats_frame.head()
+            feats_umap = umap.UMAP(n_neighbors=5, metric='correlation', min_dist=0.3).fit_transform((feats_reshaped))
+            # feats_umap = umap.UMAP().fit_transform(feats_frame)
+            this_features[id_of_labels[label]].append(feats_umap.flatten())
+            # print(feats_umap.shape)
+            # print()
+            # this_features[label].append(feats_umap)
+
             # squeeze the features
             # this_features[label].append(feats.squeeze())
     return this_features
@@ -126,15 +145,17 @@ labels = get_labels()
 nber_labels = len(labels)
 
 # initialize a dict of labels and the data set
-data, test_images = initialize_images_data(nber_labels)
+data = initialize_images_data(nber_labels, "train")
+test_images = initialize_images_data(nber_labels, "valid")
 
-features = extract_features()
+train_features = extract_features(data, "train")
+test_features = extract_features(test_images, "valid")
 
 # convert features to data frame (from dict)
 trainY = list()  # labels
 trainX = pd.DataFrame()  # images data
 
-for label, feats in features.items():
+for label, feats in train_features.items():
     for i in range(len(feats)):
         trainY.append([label]) # temp_label, ignore_index=True)
     temp_df = pd.DataFrame(feats)
@@ -156,44 +177,47 @@ trainX.head()
 
 
 # get test images features
-test_features = []
-test_images_paths = []
+testX = []
+testY = []
 
-expected_labels = []
+# test_images_paths = []
 
-for test_label, test_flowers in test_images.items():
-    basepath = "./data/" + id_of_labels[test_label]
+for test_label, test_flowers in test_features.items():
+    # basepath = "./flower_data/" + test_label
 
     for test_flower in test_flowers:
-        test_flower_path = basepath + "/" + test_flower
-        test_images_paths.append(test_flower_path)
+        # test_flower_path = basepath + "/" + test_flower
+        # test_images_paths.append(test_flower_path)
 
-        expected_labels.append(test_label)
+        testX.append(test_flower)
+        testY.append(test_label)
 
-        test_feats = get_features(test_flower_path, vgg19_model)
-        # flatten the features
-        test_features.append(test_feats.flatten())
-        # test_features.append(test_feats.squeeze())
+    # test_features.append(umap.UMAP(n_neighbors=5, metric='correlation', min_dist=0.3).
+    # fit_transform(np.reshape(test_feats, (test_feats.shape[0], -1))))
+
+    # test_features.append(test_feats.squeeze())
+    #########################
 
 # y = dataset.label
 # x = dataset.drop('label', axis=1)
 
 # classifier model
-model = MLPClassifier(hidden_layer_sizes=(50, 128, 128, 128), activation='tanh', solver='lbfgs')  # ‘identity’, ‘logistic’, ‘tanh’, # ‘lbfgs’, ‘adam’
+print("trainX: ", trainX.shape)
+model = MLPClassifier(hidden_layer_sizes=(20, 128, 128, 128), activation='identity', solver='adam')  # ‘identity’, ‘logistic’, ‘tanh’, # ‘lbfgs’, ‘adam’
 pipeline = Pipeline([('low_variance_filter', VarianceThreshold()), ('model', model)])
 pipeline.fit(trainX, trainY)
 
 # print(pipeline.(test_features))
-predicted_labels = pipeline.predict(test_features)
+predicted_labels = pipeline.predict(testX)
 
-the_score = accuracy_score(pd.DataFrame(expected_labels), predicted_labels)
+the_score = accuracy_score(testY, predicted_labels)
 print("Accuracy: ", the_score * 100, "%")
 
 f, ax = plt.subplots(1, len(test_images_paths))
-for z in range(len(test_images_paths)):
-    print(test_images_paths[z], predicted_labels[z], expected_labels[z])
-#     ax[i].imshow(Image.open(test_images_paths[i]).resize((200, 200), Image.ANTIALIAS))
-#     ax[i].text(10, 180, preds[i], color='k', backgroundcolor='red', alpha=0.8)
+for z in range(len(testY)):
+    print(test_images_paths[z], predicted_labels[z], testY[z])
+    ax[i].imshow(Image.open(test_images_paths[i]).resize((200, 200), Image.ANTIALIAS))
+    ax[i].text(10, 180, predicted_labels[i], color='k', backgroundcolor='red', alpha=0.8)
 # plt.show()
 
 
